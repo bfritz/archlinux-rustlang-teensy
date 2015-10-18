eb9a0aa7deef4dfdb676f117bdc76e29388e18b7
Brad Fritz <brad@fewerhassles.com>
2015-10-18 12:46:14 -0400

    improve on-screen installation instructions

--- zinc.sh
+++ zinc.sh
@@ -55,4 +55,8 @@ file $OUT
 arm-none-eabi-objcopy -Oihex -R .eeprom -R .fuse -R .lock -R .signature $OUT blink.hex
 cp -p blink.hex /vagrant/
 
-echo "Install with: ./teensy-loader-cli --mcu=mk20dx256 -w blink.hex"
+echo "Install with:"
+echo
+echo "    ./teensy-loader-cli --mcu=mk20dx256 -w blink.hex"
+echo
+echo "and press reset button on Teensy."
